ecf7e9e3132b7f2dcc459821d6c6ac0ae526dd92
qry <qiruiyin@126.com>
2016-12-19 15:56:09 +0800

    对象深拷贝更新
    以前的是数组的深拷贝，现在新增对象（包含数组）的深拷贝

--- demo/js好用的方法/index.js
+++ demo/js好用的方法/index.js
@@ -12,6 +12,22 @@
     } 
     return result; 
 	}
+	export function deepClone (obj) {
+	  if (Array.isArray(obj)) {
+	    return obj.map(deepClone)
+	  } else if (obj && typeof obj === 'object') {
+	    var cloned = {}
+	    var keys = Object.keys(obj)
+	    for (var i = 0, l = keys.length; i < l; i++) {
+	      var key = keys[i]
+	      cloned[key] = deepClone(obj[key])
+	    }
+	    return cloned
+	  } else {
+	    return obj
+	  }
+	}
+
 
 2、add(1)(2)(3)(4) 方法
 	function add(x) {
